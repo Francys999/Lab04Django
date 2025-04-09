9a4c07904df3465660308534e91c78018f411f15
Francys999 <francis11444@gmail.com>
2025-04-09 16:41:33 -0500

    Agregando la funcionalidad de editar y eliminar

--- src/exercise_tracker/urls.py
+++ src/exercise_tracker/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_exercise, name='add_exercise'),
+    path('edit_exercise/<int:pk>/', views.edit_exercise, name='edit_exercise'),
+    path('delete_exercise/<int:pk>/', views.delete_exercise, name='delete_exercise'),
 ]
--- src/exercise_tracker/views.py
+++ src/exercise_tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Exercise
 from .forms import ExerciseForm
 
@@ -16,3 +16,26 @@ def add_exercise(request):
         form = ExerciseForm()
     return render(request, 'exercise_tracker/add_exercise.html', {'form': form})
 
+# Vista para editar un ejercicio
+def edit_exercise(request, pk):
+    exercise = get_object_or_404(Exercise, pk=pk)
+
+    if request.method == "POST":
+        form = ExerciseForm(request.POST, instance=exercise)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # Redirigir a la página principal después de guardar
+    else:
+        form = ExerciseForm(instance=exercise)
+
+    return render(request, 'exercise_tracker/edit_exercise.html', {'form': form})
+
+# Vista para eliminar un ejercicio
+def delete_exercise(request, pk):
+    exercise = get_object_or_404(Exercise, pk=pk)
+
+    if request.method == "POST":
+        exercise.delete()
+        return redirect('home')  # Redirigir a la página principal después de eliminar
+
+    return render(request, 'exercise_tracker/delete_exercise.html', {'exercise': exercise})
